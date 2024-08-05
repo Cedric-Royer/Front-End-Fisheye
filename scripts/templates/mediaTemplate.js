158cad763134759c68ffcf7640cc0d8e02598699
Cedric Royer <cedricroyerpro@gmail.com>
2024-08-05 18:26:58 +0200

    refacto pattern factory method

--- scripts/templates/mediaTemplate.js
+++ scripts/templates/mediaTemplate.js
@@ -1,73 +1,62 @@
 import { getMediaImagePath, getMediaVideoPath } from '../utils/paths.js';
 
-function mediaTemplate(data) {
-    const { title, image, video, likes } = data;
+function addLike(likesText, mediaElement) {
+    if (!mediaElement.hasAttribute('data-liked')) {
+        let currentLikes = parseInt(likesText.textContent, 10);
+        currentLikes++;
+        likesText.textContent = `${currentLikes}`;
+        mediaElement.setAttribute('data-liked', 'true');
+    }
+}
 
+function createTitleLikesContainer(title, likes, mediaElement) {
+    const container = document.createElement('div');
+    container.classList.add('title-likes-container');
+
+    const mediaTitle = document.createElement('h2');
+    mediaTitle.textContent = title;
+    container.appendChild(mediaTitle);
+
+    const mediaLikes = document.createElement('p');
+    const likesText = document.createElement('span');
+    likesText.textContent = `${likes}`;
+    const likesIcon = document.createElement('button');
+    likesIcon.classList.add('fa-solid', 'fa-heart', 'icon-like');
+    likesIcon.setAttribute("aria-label", "j'aime");
+    mediaLikes.appendChild(likesText);
+    mediaLikes.appendChild(likesIcon);
+    container.appendChild(mediaLikes);
+
+    likesIcon.addEventListener('click', () => addLike(likesText, mediaElement));
+
+    return container;
+}
+
+function createImageMediaModel(data) {
+    const { title, image, likes } = data;
     const mediaImagePath = getMediaImagePath(image);
-    const mediaVideoPath = getMediaVideoPath(video);
 
     function getMediaElement(index) {
         const mediaElement = document.createElement('div');
         mediaElement.classList.add('media-item');
 
-        if (image) {
-            const link = document.createElement('a');
-            link.href = '#';
-            link.setAttribute("aria-label", `"${title}": voir l'image originale.`);
-            link.addEventListener('click', (event) => {
-                event.preventDefault();
-                window.openCarousel(index);
-            });
-
-            const img = document.createElement('img');
-            img.src = mediaImagePath;
-            img.alt = title;
-            img.loading = 'lazy';
-
-            link.appendChild(img);
-            mediaElement.appendChild(link);
-        } else if (video) {
-            const link = document.createElement('a');
-            link.href = '#';
-            link.setAttribute("aria-label", `"${title}": voir la vidéo.`);
-            link.addEventListener('click', (event) => {
-                event.preventDefault();
-                window.openCarousel(index);
-            });
-
-            const videoElement = document.createElement('video');
-            videoElement.src = mediaVideoPath;
-            videoElement.controls = false;
-            videoElement.setAttribute("tabindex","-1");
-            link.appendChild(videoElement);
-            mediaElement.appendChild(link);
-        }
-
-        const titleLikesContainer = document.createElement('div');
-        titleLikesContainer.classList.add('title-likes-container');
-
-        const mediaTitle = document.createElement('h2');
-        mediaTitle.textContent = title;
-        titleLikesContainer.appendChild(mediaTitle);
-
-        const mediaLikes = document.createElement('p');
-        const likesText = document.createElement('span');
-        likesText.classList.add("like-number");
-        likesText.textContent = `${likes}`;
-        const likesIcon = document.createElement('button');
-        likesIcon.classList.add('fa-solid', 'fa-heart', 'icon-like');
-        likesIcon.setAttribute("aria-label", "j'aime");
-        mediaLikes.appendChild(likesText);
-        mediaLikes.appendChild(likesIcon);
-
-        titleLikesContainer.appendChild(mediaLikes);
-
-        likesIcon.addEventListener('click', (event) => {
+        const link = document.createElement('a');
+        link.href = '#';
+        link.setAttribute("aria-label", `"${title}": voir l'image originale.`);
+        link.addEventListener('click', (event) => {
             event.preventDefault();
-            let currentLikes = parseInt(likesText.textContent);
-            currentLikes++;
-            likesText.textContent = `${currentLikes}`;
-        });     
+            window.openCarousel(index);
+        });
+
+        const img = document.createElement('img');
+        img.src = mediaImagePath;
+        img.alt = title;
+        img.loading = 'lazy';
+
+        link.appendChild(img);
+        mediaElement.appendChild(link);
+
+        const titleLikesContainer = createTitleLikesContainer(title, likes, mediaElement);
 
         mediaElement.appendChild(titleLikesContainer);
 
@@ -75,6 +64,47 @@ function mediaTemplate(data) {
     }
 
     return { getMediaElement };
+}
+
+function createVideoMediaModel(data) {
+    const { title, video, likes } = data;
+    const mediaVideoPath = getMediaVideoPath(video);
+
+    function getMediaElement(index) {
+        const mediaElement = document.createElement('div');
+        mediaElement.classList.add('media-item');
+
+        const link = document.createElement('a');
+        link.href = '#';
+        link.setAttribute("aria-label", `"${title}": voir la vidéo.`);
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            window.openCarousel(index);
+        });
+
+        const videoElement = document.createElement('video');
+        videoElement.src = mediaVideoPath;
+        videoElement.controls = false;
+
+        link.appendChild(videoElement);
+        mediaElement.appendChild(link);
+
+        const titleLikesContainer = createTitleLikesContainer(title, likes, mediaElement);
+
+        mediaElement.appendChild(titleLikesContainer);
+
+        return mediaElement;
+    }
+
+    return { getMediaElement };
+}
+
+function mediaTemplate(data) {
+    if (data.image) {
+        return createImageMediaModel(data);
+    } else if (data.video) {
+        return createVideoMediaModel(data);
+    } 
 }
 
 export default mediaTemplate;
